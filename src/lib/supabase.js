83ba6439a1d11dddc92a12ccdbbeddc06bedf3af
Aswin tejasvi S <150126471+aswin-dmg@users.noreply.github.com>
2026-05-08 16:28:10 +0530

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://dsyahjvzinbnmfopcxej.supabase.co'
-const supabaseAnonKey = 'sb_publishable_rM90_FxFZ_gFsJpqRGmRgg_QCjn8PT2'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
